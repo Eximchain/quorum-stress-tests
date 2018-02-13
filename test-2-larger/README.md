103edeaa68680e21a26f8b1d3da8642de4f41a26
Louis <loulamia@mit.edu>
2018-02-13 14:44:09 -0500

    Start running test 2

---
+++ test-2-larger/README.md
@@ -0,0 +1,33 @@
+# Test 2: Increased Size
+  
+## Description
+
+Tests a larger, multi-region network to determine if a network of this size can stand up.
+
+## Network Properties
+
+### Network Size
+| Property             | Value |
+| :------------------: | :---: |
+| Number of Regions    | 14    |
+| Number of Makers     | 34    |
+| Number of Validators | 21    |
+| Number of Observers  | 1     |
+
+### Network Configuration
+| Property                       | Value       |
+| :----------------------------: | :---------: |
+| Votes to Confirm Block         | 11          |
+| Gas Limit Per Block            | 804247552   |
+| Block Maker Minimum Block Time | 60 seconds  |
+| Block Maker Maximum Block Time | 300 seconds |
+
+### Instance Types
+| Property                  | Value     |
+| :-----------------------: | :-------: |
+| Quorum Node Instance Type | t2.medium |
+| Bootnode Instance Type    | t2.small  |
+
+## Test Results
+
+**TODO**: Test still in progress. Results will be uploaded upon completion.
---
+++ test-2-larger/terraform/main.tf
@@ -0,0 +1,103 @@
+module "quorum_cluster" {
+  source = "github.com/Eximchain/terraform-aws-quorum-cluster.git//terraform/modules/quorum-cluster?ref=v0.1.3-beta"
+
+  # AMI variables sourced from amis.auto.tfvars.json
+  quorum_amis   = "${var.quorum_amis}"
+  vault_amis    = "${var.vault_amis}"
+  bootnode_amis = "${var.bootnode_amis}"
+
+  # Common variables sourced from terraform.tfvars
+  public_key_path  = "${var.public_key_path}"
+  private_key_path = "${var.private_key_path}"
+  cert_owner       = "${var.cert_owner}"
+
+  force_destroy_s3_buckets = "${var.force_destroy_s3_buckets}"
+  generate_metrics         = "${var.generate_metrics}"
+
+  vault_cluster_size   = "${var.vault_cluster_size}"
+  vault_instance_type  = "${var.vault_instance_type}"
+  consul_cluster_size  = "${var.consul_cluster_size}"
+  consul_instance_type = "${var.consul_instance_type}"
+
+  # Cluster specific variables
+  network_id = 991
+
+  gas_limit      = 804247552
+  vote_threshold = 11
+  min_block_time = 60
+  max_block_time = 300
+
+  primary_region = "us-east-1"
+
+  bootnode_instance_type    = "t2.small"
+  quorum_node_instance_type = "t2.medium"
+
+# 14 Bootnodes across 14 Regions
+  bootnode_counts       = {
+    us-east-1      = 1
+    us-east-2      = 1
+    us-west-1      = 1
+    us-west-2      = 1
+    eu-central-1   = 1
+    eu-west-1      = 1
+    eu-west-2      = 1
+    ap-south-1     = 1
+    ap-northeast-1 = 1
+    ap-northeast-2 = 1
+    ap-southeast-1 = 1
+    ap-southeast-2 = 1
+    ca-central-1   = 1
+    sa-east-1      = 1
+  }
+# 34 Makers across 14 Regions
+  maker_node_counts     = {
+    us-east-1      = 3
+    us-east-2      = 2
+    us-west-1      = 2
+    us-west-2      = 4
+    eu-central-1   = 2
+    eu-west-1      = 3
+    eu-west-2      = 2
+    ap-south-1     = 3
+    ap-northeast-1 = 2
+    ap-northeast-2 = 2
+    ap-southeast-1 = 2
+    ap-southeast-2 = 3
+    ca-central-1   = 2
+    sa-east-1      = 2
+  }
+# 21 Validators across 14 Regions
+  validator_node_counts = {
+    us-east-1      = 2
+    us-east-2      = 3
+    us-west-1      = 1
+    us-west-2      = 2
+    eu-central-1   = 2
+    eu-west-1      = 1
+    eu-west-2      = 1
+    ap-south-1     = 1
+    ap-northeast-1 = 1
+    ap-northeast-2 = 1
+    ap-southeast-1 = 2
+    ap-southeast-2 = 1
+    ca-central-1   = 2
+    sa-east-1      = 1
+  }
+# 1 Observer across 1 Region
+  observer_node_counts  = {
+    us-east-1      = 1
+    us-east-2      = 0
+    us-west-1      = 0
+    us-west-2      = 0
+    eu-central-1   = 0
+    eu-west-1      = 0
+    eu-west-2      = 0
+    ap-south-1     = 0
+    ap-northeast-1 = 0
+    ap-northeast-2 = 0
+    ap-southeast-1 = 0
+    ap-southeast-2 = 0
+    ca-central-1   = 0
+    sa-east-1      = 0
+  }
+}
